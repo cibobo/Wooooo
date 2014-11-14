0bf86e874fded5d398285c6f833f449bb3859d99
cibobo <cibobo2005@gmail.com>
2014-11-14 23:15:12 +0100

    Add location service from Google Play API

---
+++ TestService/app/src/main/java/com/example/cibobo/testservice/LocationService.java
@@ -0,0 +1,242 @@
+package com.example.cibobo.testservice;
+
+import android.app.Dialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.ErrorDialogFragment;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
+
+public class LocationService extends Service implements
+        LocationListener,
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener{
+
+    // A request to connect to Location Services
+    private LocationRequest mLocationRequest;
+
+    // Stores the current instantiation of the location client in this object
+    private LocationClient mLocationClient;
+
+    // Handles to UI widgets
+    private TextView mLatLng;
+    private TextView mAddress;
+    private ProgressBar mActivityIndicator;
+    private TextView mConnectionState;
+    private TextView mConnectionStatus;
+
+    /*
+     * Note if updates have been turned on. Starts out as "false"; is set to "true" in the
+     * method handleRequestSuccess of LocationUpdateReceiver.
+     *
+     */
+    boolean mUpdatesRequested = false;
+
+    LocationBinder locationBinder = new LocationBinder();
+
+    private final String tag = "Location Service";
+
+    private Context context;
+
+    public class LocationBinder extends Binder {
+        public void startLocationService(Context inputContext){
+            context = inputContext;
+            /*
+             * Connect the client. Don't re-start any requests here;
+             * instead, wait for onResume()
+             */
+             mLocationClient.connect();
+        }
+    }
+
+    public LocationService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Create a new global location parameters object
+        mLocationRequest = LocationRequest.create();
+
+        /*
+         * Set the update interval
+         */
+        mLocationRequest.setInterval(LocationUtils.UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Use high accuracy
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Set the interval ceiling to one minute
+        mLocationRequest.setFastestInterval(LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS);
+
+        // Note that location updates are off until the user turns them on
+        mUpdatesRequested = false;
+
+
+        /*
+         * Create a new location client, using the enclosing class to
+         * handle callbacks.
+         */
+        mLocationClient = new LocationClient(context, this, this);
+    }
+
+    @Override
+    public void onDestroy() {
+        // If the client is connected
+        if (mLocationClient.isConnected()) {
+            stopPeriodicUpdates();
+        }
+
+        // After disconnect() is called, the client is considered "dead".
+        mLocationClient.disconnect();
+
+        super.onDestroy();
+    }
+
+    /*
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(tag, "Client Connected");
+        if (mUpdatesRequested) {
+            startPeriodicUpdates();
+        }
+    }
+
+    /*
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+    @Override
+    public void onDisconnected() {
+        Log.d(tag, "Client Disconnected");
+    }
+
+
+    /**
+     * Report location updates to the UI.
+     *
+     * @param location The updated location.
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(tag, "Location changed");
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+/*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            Log.e(tag, "Have solution for connection failed");
+        } else {
+
+            // If no resolution is available, display a dialog to the user with the error.
+            Log.e(tag, "No solution for connection failed");
+        }
+    }
+
+    /**
+     * In response to a request to start updates, send a request
+     * to Location Services
+     */
+    private void startPeriodicUpdates() {
+        mLocationClient.requestLocationUpdates(mLocationRequest, (com.google.android.gms.location.LocationListener) LocationService.this);
+        Log.i(tag, "Period update start");
+    }
+
+    /**
+     * In response to a request to stop updates, send a request to
+     * Location Services
+     */
+    private void stopPeriodicUpdates() {
+        mLocationClient.removeLocationUpdates((com.google.android.gms.location.LocationListener) LocationService.this);
+        Log.i(tag, "Period update stopped");
+    }
+
+    /**
+     * Verify that Google Play services is available before making a request.
+     *
+     * @return true if Google Play services is available, otherwise false
+     */
+    private boolean servicesConnected() {
+
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d(LocationUtils.APPTAG, getString(R.string.play_services_available));
+
+            // Continue
+            return true;
+            // Google Play services was not available for some reason
+        } else {
+            Log.e(tag, "Google Play can't connect");
+            return false;
+        }
+    }
+
+    public String getLocation(){
+        // If Google Play Services is available
+        if (servicesConnected()) {
+
+            // Get the current location
+            Location currentLocation = mLocationClient.getLastLocation();
+
+            // Display the current location in the UI
+            return LocationUtils.getLatLng(context, currentLocation);
+        } else {
+            return null;
+        }
+    }
+}
